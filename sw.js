2049d06c2cf2bdaf66ee7bb25065ee22191c5aea
tiagobandeira <tiagobandeirabarros@gmail.com>
2024-03-01 22:28:32 -0300

    fix:adicionado service worker para solucionar CORS

---
+++ sw.js
@@ -0,0 +1,37 @@
+self.addEventListener("install", function () {
+    self.skipWaiting();
+  });
+  
+  self.addEventListener("activate", (event) => {
+    event.waitUntil(self.clients.claim());
+  });
+  
+  self.addEventListener("fetch", function (event) {
+    if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
+      return;
+    }
+  
+    event.respondWith(
+      fetch(event.request)
+        .then(function (response) {
+            
+          if (!response.url.includes("template.html")) return response;
+  
+          const newHeaders = new Headers(response.headers);
+          newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
+          newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
+  
+          const moddedResponse = new Response(response.body, {
+            status: response.status,
+            statusText: response.statusText,
+            headers: newHeaders,
+          });
+  
+          return moddedResponse;
+        })
+        .catch(function (e) {
+          console.error(e);
+        })
+    );
+  });
+  
