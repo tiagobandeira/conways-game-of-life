eb2895fda22caa925c83e6a4d09d28a4fe7dc3ab
tiagobandeira <tiagobandeirabarros@gmail.com>
2024-03-10 19:31:17 -0300

    perf: tentativa de melhora no carregamento dos icones do botoes

--- scripts/buttonLoading.js
+++ scripts/buttonLoading.js
@@ -1,10 +1,16 @@
 var btnLoaders = document.querySelectorAll(".btn-loader")
-
+var icons = document.querySelectorAll(".fa-solid")
+var iconGrid = document.querySelector(".icon-grid")
 window.addEventListener("DOMContentLoaded",()=>{
-
+    btnLoaders.forEach(btn => {
+        btn.style.display = "block";
+    });
+    iconGrid.style.display = "block";
     setTimeout(() => {
-        btnLoaders.forEach(btn => {
-            btn.style.display = "block";
+        icons.forEach(icon => {
+            icon.style.scale = "1";
         });
+        iconGrid.style.scale = "1";
+       
     }, 100);  
 })
